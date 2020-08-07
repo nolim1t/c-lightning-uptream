6e8d250e991033342f80a0c1ecc45e3f33f3503f
nolim1t <hello@nolim1t.co>
2020-08-08 03:37:58 +0700

    fix strip components

--- fetch-bitcoin.sh
+++ fetch-bitcoin.sh
@@ -10,5 +10,5 @@ wget -qO bitcoin.asc "$BITCOIN_ASC_URL"
 grep $BITCOIN_TARBALL bitcoin.asc | tee SHA256SUMS.asc
 sha256sum -c SHA256SUMS.asc
 BD=bitcoin-$BITCOIN_VERSION/bin
-tar -xzvf $BITCOIN_TARBALL $BD/bitcoin-cli --strip
+tar -xzvf $BITCOIN_TARBALL $BD/bitcoin-cli --strip-components=1
 rm $BITCOIN_TARBALL
